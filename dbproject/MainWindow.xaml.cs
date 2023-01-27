9a3e78c7aef2b76f1f00ccd7ef658fc71d9b30e5
SaraM2000 <saramovahhed@yahoo.com>
2023-01-27 13:30:12 +0330

    last changes

--- dbproject/MainWindow.xaml.cs
+++ dbproject/MainWindow.xaml.cs
@@ -12,19 +12,68 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data.Odbc;
+using System.Data.Common;
+using Microsoft.Data.Tools.Schema.Sql.UnitTesting.Configuration;
 
 namespace dbproject
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
+    
+    
+   
     public partial class MainWindow : Window
     {
+
+       
+       
+
+
+
         public MainWindow()
         {
             InitializeComponent();
+   
         }
+        
 
+        private void ConfirmBtn(object sender, RoutedEventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-UUJ3DJ6;Initial Catalog=TravelAgency;Integrated Security=True");
+            SqlCommand com = new SqlCommand("Procedure_InsEmployee", con) { CommandType = System.Data.CommandType.StoredProcedure };
+            con.Open();
+            com.Parameters.AddWithValue("@agencyBranchCode", agencyBranchCode.Text); ///////// list
+            com.Parameters.AddWithValue("@employeeNationalCode", NationalCode.Text);
+            com.Parameters.AddWithValue("@employeeFirstName", FirstName.Text);
+            com.Parameters.AddWithValue("@employeeLastName", LastName.Text);
+            com.Parameters.AddWithValue("@employeeBirthDate", BirthDate.Text); ////////// date
+            com.Parameters.AddWithValue("@Employee_Address", Address.Text);
+            com.Parameters.AddWithValue("@employeePhoneNum", PhoneNumber.Text);
+            com.Parameters.AddWithValue("@employeeTelNum", TelNumber.Text);
+            com.Parameters.AddWithValue("@Employee_Email", Email.Text);
+            com.Parameters.AddWithValue("@employeeBankAccNum", BAInfo.Text);
+            com.Parameters.AddWithValue("@employeeFatherName", FathersName.Text);
+            com.Parameters.AddWithValue("@employeeFamNum", FamilyNumber.Text);
+            com.Parameters.AddWithValue("@employeeStartDate", StartDate.Text);/////// date
+            com.Parameters.AddWithValue("@employeeMaritalStatus", MartialStatus.Text);/// list
+            com.Parameters.AddWithValue("@employeeChildNum", ChildNumber.Text);
+            com.Parameters.AddWithValue("@employeeEndDate", EndDate.Text); /////////// date
+            com.Parameters.AddWithValue("@Employee_Education", Education.Text);
+            com.Parameters.AddWithValue("@employeeWorkExpe", WorkExpe.Text);
+            com.Parameters.AddWithValue("@employeePreWorkPlace", PreWorkPlace.Text);
+            com.Parameters.AddWithValue("@employeePreWorkAddress", PreWorkAddress.Text);
+            com.Parameters.AddWithValue("@employeePreWorkTelNum", PreWorkTelNum.Text);
+            com.Parameters.AddWithValue("@employeeSabbaticalDays", Sebbaticaldays.Text);
+            com.Parameters.AddWithValue("@Employee_Position", Position.Items);/////// list
+           
+            com.ExecuteNonQuery();
+            
+        }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
@@ -34,5 +83,10 @@ namespace dbproject
         {
 
         }
+
+        
+
+
+
     }
 }
---
+++ dbproject/Window3.xaml.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace dbproject
+{
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary> 
+
+    public partial class Window3 : Window
+    {
+
+
+        public class Employee
+        {
+
+            public String FirstName { get; set; }
+            public String LastName { get; set; }
+            public int NationalCode { get; set; }
+            public int PhoneNumber { get; set; }
+            public string Email { get; set; }
+            public int TelNumber { get; set; }
+            public string BirthDate { get; set; }
+            public String? Address { get; set; }
+            public String FatherName { get; set; }
+            public String MaritalStatus { get; set; }
+            public String StartDate { get; set; }
+            public String EndDate { get; set; }
+            public int BankAccNum { get; set; }
+            public int ChildNum { get; set; }
+            public int FamNum { get; set; }
+            public int SebbaticalDays { get; set; }
+            public String Position { get; set; }
+
+            public String Education { get; set; }
+            public int WorkExpe { get; set; }
+            public String PreWorkPlace { get; set; }
+            public String PreWordAddress { get; set; }
+            public String PreWordTelNum { get; set; }
+            public String AgencyBranchCode { get; set; }
+
+            public String Resume { get; set; }
+
+
+
+
+
+        }
+
+
+
+        public Window3()
+        {
+            InitializeComponent();
+
+            DataGrid datagrid = new DataGrid();
+
+
+
+
+            var ListOfemployees = new List<Employee>();
+            {
+
+                new Employee() { FirstName = "sara", LastName = "movahhed" };
+
+
+            }
+
+
+
+            /*
+             private void FNfocus (object sender, RoutedEventArgs e)
+             {
+
+                FN.Text = "";
+
+
+
+             }
+            private void LNfocus(object sender, RoutedEventArgs e)
+            {
+
+
+                LN.Text = "";
+
+
+
+            }
+
+            private void NCfocus(object sender, RoutedEventArgs e)
+            {
+
+
+                NC.Text = "";
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+            private void ScrollBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+            {
+
+            }
+
+            private void Border_Drop(object sender, DragEventArgs e)
+            {
+
+            }
+
+            private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+            {
+
+
+            }
+
+            private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+            {
+
+            }
+
+            private void Button_Click(object sender, RoutedEventArgs e)
+            {
+
+            } */
+        }
+        
+        private void Delete_Btn_MouseEnter(object sender, MouseEventArgs e)
+        {
+            var converter = new System.Windows.Media.BrushConverter();
+            var brush = (Brush) converter.ConvertFromString("#FFF7F1FF");
+            Delete_Btn.Background = brush;
+        }
+
+        private void Update_Btn_MouseEnter(object sender, MouseEventArgs e)
+        {
+            var converter = new System.Windows.Media.BrushConverter();
+            var brush = (Brush)converter.ConvertFromString("#FFF7F1FF");
+            Delete_Btn.Background = brush;
+        }
+    }
+}
